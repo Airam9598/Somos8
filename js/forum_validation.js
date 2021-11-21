dc7f1ee4181010459b83e0cfe18e8e1780f4a3e4
kiowa <k.andueza96@gmail.com>
2021-11-21 13:39:47 +0000

    Modificaciones en las validaciones de suggestion y forum

--- js/forum_validation.js
+++ js/forum_validation.js
@@ -2,73 +2,29 @@ $(document).ready(function() {
     const form = document.getElementById('forumform');
     const inputs = document.querySelectorAll('#forumform');
 
-    const pattern = {
-        place: /^[a-zA-Z.,À-ÿ\s]{4,30}$/,
-        date: /^\d{2}[/]\d{2}[/]\d{4}$/,
-        time: /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/,
-        material: /^[a-zA-Z.,0-9À-ÿ\s]{5,150}$/,
-        description: /^[a-zA-Z0-9.,À-ÿ\s]{10,320}$/
-    }
+    const mapPattern = new Map()
+    mapPattern.set("place", "^[a-zA-Z.,À-ÿ\\s]{4,30}$")
+
+    mapPattern.set("time", "^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$")
+    mapPattern.set("material", "^[a-zA-Z.,0-9À-ÿ\\s]{5,150}$")
+    mapPattern.set("description", "^[a-zA-Z0-9.,À-ÿ\\s]{10,320}$")
 
 
     const validateForm = (e) => {
-        switch (e.target.name) {
-            case "place":
-                validateField(pattern.place, e.target, 'place');
-            break;
-            case "date":
-                validateField(pattern.date, e.target, 'date');
-            break;
-            case "time":
-                validateField(pattern.time, e.target, 'time');
-            break;
-            case "material":
-                validateField(pattern.material, e.target, 'material');
-            break;
-            case "description":
-                validateField(pattern.description, e.target, 'description');
-            break;
-        }
+        var prop = e.target.name
+        validateField(mapPattern.get(prop), e.target, e.target.id)
     }
 
     const validateField = (pattern, input, field) => {
-        if(pattern.test(input.value)){
-            document.getElementById(field).classList.remove('is-invalid');
-            document.getElementById(field).classList.add('is-valid');
-            document.getElementById(field).classList.remove('text-danger');
-            document.getElementById(field).classList.add('text-success');
-            if (field == "place") {
-                document.getElementById('place-error').innerHTML = "";
-            } else if (field == "date") {
-                document.getElementById('date-error').innerHTML = "";
-            } else if (field == "time") {
-                document.getElementById('time-error').innerHTML = "";
-            } else if (field == "material") {
-                document.getElementById('material-error').innerHTML = "";
-            } else if (field == "description") {
-                document.getElementById('description-error').innerHTML = "";
-            }
+        if(new RegExp(pattern).test(input.value)) {
+            document.getElementById(field).classList.remove("is-invalid","text-danger", "border" ,"border-danger");
+            document.getElementById(field).classList.add("is-valid", "text-success", "border", "border-success");
+            document.getElementById(`${field}-error`).style.display = 'none';
+
         } else{
-            document.getElementById(field).classList.remove('is-valid');
-            document.getElementById(field).classList.add('is-invalid');
-            document.getElementById(field).classList.remove('text-success');
-            document.getElementById(field).classList.add('text-danger');
-            if (field == "place") {
-                document.getElementById('place-error').innerHTML = "El lugar no debe contener caracteres especiales. Máx. de 4-29 caracteres.";
-                document.getElementById('place-error').style.display = "block";
-            } else if (field == "date") {
-                document.getElementById('date-error').innerHTML = "Campo obligatorio*";
-                document.getElementById('date-error').style.display = "block";
-            } else if (field == "time") {
-                document.getElementById('time-error').innerHTML = "Campo obligatorio*";
-                document.getElementById('time-error').style.display = "block";
-            } else if (field == "material") {
-                document.getElementById('material-error').innerHTML = "No debe contener caracteres especiales. Máx. de 5-150 caracteres.";
-                document.getElementById('material-error').style.display = "block";
-            } else if (field == "description") {
-                document.getElementById('description-error').innerHTML = "La descripción no debe contener caracteres especiales. Máx. de 10-320 caracteres.";
-                document.getElementById('description-error').style.display = "block";
-            }
+            document.getElementById(field).classList.remove('is-valid', 'text-success', "border", "border-success");
+            document.getElementById(field).classList.add('is-invalid', 'text-danger', "border", "border-danger");
+            document.getElementById(`${field}-error`).style.display = 'block';
         }
     }
 
@@ -78,8 +34,8 @@ $(document).ready(function() {
     });
 
     function updateimage(e){
-        var file=e.target.files[0];
-        var reader  = new FileReader();
+        const file = e.target.files[0];
+        const reader  = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function(e)  {
             document.getElementById('forum_image').src = e.target.result;
@@ -87,5 +43,16 @@ $(document).ready(function() {
     }
 
     document.getElementById('img-change2').addEventListener("change", updateimage);
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        Swal.fire({
+            icon: 'success',
+            title: '¡Su excursión ha sido publicada correctamente!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+
+    });
 
 });
--- js/suggestions_validation.js
+++ js/suggestions_validation.js
@@ -1,65 +1,50 @@
 $(document).ready(function() {
+
     const form = document.getElementById('suggestions');
     const inputs = document.querySelectorAll('#suggestions');
 
-    const pattern = {
-        name: /^[a-zA-ZÀ-ÿ\s]{4,29}$/,
-        email: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,10}$/,
-        text: /^[a-zA-Z0-9.,À-ÿ\s]{12,250}$/
-    }
 
+    const mapPattern = new Map()
+    mapPattern.set("name", "^[a-zA-ZÀ-ÿ\\s]{4,29}$")
+    mapPattern.set("mail", "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,10}$")
+    mapPattern.set("text", "^[a-zA-Z0-9.,À-ÿ\\s]{12,250}$")
 
     const validateForm = (e) => {
-        switch (e.target.name) {
-            case "name":
-                validateField(pattern.name, e.target, 'name');
-                break;
-            case "mail":
-                validateField(pattern.email, e.target, 'mail-suggestion');
-                break;
-            case "text":
-                validateField(pattern.text, e.target, 'textarea');
-                break;
-        }
+        var prop = e.target.name
+        validateField(mapPattern.get(prop), e.target, e.target.id)
     }
 
     const validateField = (pattern, input, field) => {
-        if (pattern.test(input.value)) {
-            document.getElementById(field).classList.remove('is-invalid');
-            document.getElementById(field).classList.add('is-valid');
-            document.getElementById(field).classList.remove('text-danger');
-            document.getElementById(field).classList.add('text-success');
-            if (field == "name") {
-                document.getElementById('name-error').innerHTML = "";
-            } else if (field == "mail-suggestion") {
-                document.getElementById('mail-error').innerHTML = "";
-            } else if (field == "textarea") {
-                document.getElementById('suggestion-error').innerHTML = "";
-            }
+        if(new RegExp(pattern).test(input.value)) {
+            document.getElementById(field).classList.remove("is-invalid","text-danger", "border" ,"border-danger");
+            document.getElementById(field).classList.add("is-valid", "text-success", "border", "border-success");
+            document.getElementById(`${field}-error`).style.display = 'none';
 
-        } else {
-            document.getElementById(field).classList.remove('is-valid');
-            document.getElementById(field).classList.add('is-invalid');
-            document.getElementById(field).classList.remove('text-success');
-            document.getElementById(field).classList.add('text-danger');
-            if (field == "name") {
-                document.getElementById('name-error').innerHTML = "Solo puede contener letras y espacios. De 4-29 caracteres.";
-                document.getElementById('name-error').style.display = "block";
-            } else if (field == "mail-suggestion") {
-                document.getElementById('mail-error').innerHTML = "La dirección de correo no es válida. Ej: example@ulpgc.es";
-                document.getElementById('mail-error').style.display = "block";
-            } else if (field == "textarea") {
-                document.getElementById('suggestion-error').innerHTML = "Texto no válido. Inserte de 12-250 caracteres.";
-                document.getElementById('suggestion-error').style.display = "block";
-            }
+        } else{
+            document.getElementById(field).classList.remove('is-valid', 'text-success', "border", "border-success");
+            document.getElementById(field).classList.add('is-invalid', 'text-danger', "border", "border-danger");
+            document.getElementById(`${field}-error`).style.display = 'block';
         }
     }
 
-
-    inputs.forEach((input) => {
+    inputs.forEach((input) =>{
         input.addEventListener('keyup', validateForm);
         input.addEventListener('blur', validateForm);
     });
-});
 
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        Swal.fire({
+            icon: 'success',
+            title: '¡Su sugerencia ha sido enviada correctamente!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+
+    });
+
+
+
+});
 
