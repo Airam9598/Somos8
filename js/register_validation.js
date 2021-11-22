b8e0af31e482b1069b3e44aa4e12d256df1bbae7
Airam <82757584+Airam9598@users.noreply.github.com>
2021-11-22 16:46:16 +0000

    Mejoras varias en validación

--- js/register_validation.js
+++ js/register_validation.js
@@ -2,83 +2,67 @@ $(document).ready(function() {
 
     const form = document.getElementById('registers');
     const inputs = document.querySelectorAll('#registers input');
-    const pattern = {
-        psw: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}/,
-        email: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,10}$/,
-        name: /^[a-zA-ZÀ-ÿ\s]{2,29}$/
-    }
+
+    const mapPattern = new Map()
+    mapPattern.set("mail", '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,10}$')
+    mapPattern.set("psw2", '(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}')
+    mapPattern.set("psw", '(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}')
+    mapPattern.set("name", '^[a-zA-ZÀ-ÿ\\s]{2,29}$')
+    mapPattern.set("name2", '\\s+')
 
     const validateForm = (e) => {
-        switch (e.target.name) {
-            case "mail":
-                validateField(pattern.email, e.target, 'mail-register');
-                break;
-            case "psw":
-                validateField(pattern.psw, e.target, 'password-register');
-                break;
-            case "psw2":
-                validateField(pattern.psw, e.target, 'password-confirm');
-                break;
-            case "name":
-                validateField(pattern.name, e.target, 'name-register');
-                break;
-        }
+        let prop = e.target.name
+        validateField(mapPattern.get(prop), e.target, e.target.id)
     }
 
     const validateField = (pattern, input, field) => {
-        if (pattern.test(input.value)) {
-            document.getElementById(field).classList.remove('is-invalid');
-            document.getElementById(field).classList.add('is-valid');
-            if (field == "password-register") {
-                document.getElementById('psw-error').style.display = "none";
-                if (document.getElementById('password-register').value != "" && document.getElementById('password-confirm').value != "") {
-                    if (document.getElementById('password-register').value != document.getElementById('password-confirm').value) {
-                        document.getElementById(field).classList.remove('is-valid');
-                        document.getElementById(field).classList.add('is-invalid');
-                        document.getElementById('psw-error').innerHTML = "Las contraseñas no coinciden";
-                        document.getElementById('psw-error').style.display = "block";
+        if(new RegExp(pattern).test(input.value)) {
+            document.getElementById(field).classList.remove("is-invalid","text-danger", "border" ,"border-danger");
+            document.getElementById(field).classList.add("is-valid", "text-success", "border", "border-success");
+            document.getElementById(`${field}-error`).style.display = 'none';
+            if (field.includes("psw")) {
+                if (document.getElementById('psw').value != "" && document.getElementById('psw2').value != "") {
+                    if (document.getElementById('psw').value != document.getElementById('psw2').value) {
+                        document.getElementById(field).classList.remove('is-valid', 'text-success', "border", "border-success");
+                        document.getElementById(field).classList.add('is-invalid', 'text-danger', "border", "border-danger");
+                        document.getElementById(`${field}-error`).innerHTML = "Las contraseñas no coinciden";
+                        document.getElementById(`${field}-error`).style.display = "block";
+                    }else{
+                        document.getElementById(`psw-error`).style.display = "none";
+                        document.getElementById(`psw2-error`).style.display = "none";
                     }
                 }
-            } else if(field == "password-confirm"){
-                document.getElementById('psw2-error').style.display = "none";
-                if (document.getElementById('password-register').value != "" && document.getElementById('password-confirm').value != "") {
-                    if (document.getElementById('password-register').value != document.getElementById('password-confirm').value) {
-                        document.getElementById(field).classList.remove('is-valid');
-                        document.getElementById(field).classList.add('is-invalid');
-                        document.getElementById('psw2-error').innerHTML = "Las contraseñas no coinciden";
-                        document.getElementById('psw2-error').style.display = "block";
-                    }
+            }else if (field.includes("name")) {
+                if (new RegExp(mapPattern.get("name2")).test(input.value)) {
+                    document.getElementById(field).classList.remove('is-valid', 'text-success', "border", "border-success");
+                    document.getElementById(field).classList.add('is-invalid', 'text-danger', "border", "border-danger");
+                    document.getElementById(`${field}-error`).style.display = "block";
                 }
-            }else if (field == "name-register") {
-                var noValido = /\s/;
-                if (noValido.test(document.getElementById('name-register').value)) {
-                    document.getElementById(field).classList.remove('is-valid');
-                    document.getElementById(field).classList.add('is-invalid');
-                    document.getElementById('error').innerHTML = "El nombre de usuario no puede contener espacios";
-                    document.getElementById('error').style.display = "block";
-                }
-            }else if (field == "mail-register") {
-                document.getElementById('maillogin-error').style.display="none";
             }
         } else {
-            document.getElementById(field).classList.remove('is-valid');
-            document.getElementById(field).classList.add('is-invalid');
-            if (field == "password-register") {
-                document.getElementById('psw-error').innerHTML = "La contraseña debe contener al menos un número, una letra mayúscula y minúscula y más de 6 carácteres";
-                document.getElementById('psw-error').style.display = "block";
-            } else if(field == "password-confirm"){
-                document.getElementById('psw2-error').innerHTML = "La contraseña debe contener al menos un número, una letra mayúscula y minúscula y más de 6 carácteres";
-                document.getElementById('psw2-error').style.display = "block";
-            } else if (field == "mail-register") {
-                document.getElementById('maillogin-error').innerHTML = "La dirección de correo no es válida. Ej: example@ulpgc.es";
-                document.getElementById('maillogin-error').style.display = "block";
+            document.getElementById(field).classList.remove('is-valid', 'text-success', "border", "border-success");
+            document.getElementById(field).classList.add('is-invalid', 'text-danger', "border", "border-danger");
+            if (field.includes("psw")) {
+                document.getElementById(`${field}-error`).innerHTML = "La contraseña debe contener al menos un número, una letra mayúscula y minúscula y más de 6 carácteres";
             }
+            document.getElementById(`${field}-error`).style.display = 'block';
         }
     }
 
     inputs.forEach((input) => {
         input.addEventListener('keyup', validateForm);
         input.addEventListener('blur', validateForm);
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        Swal.fire({
+            icon: 'success',
+            title: '¡Bienvenid@ a somos8!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+
     });
 });
 
